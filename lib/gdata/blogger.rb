d2f3510bff0e5ab4ed1e6f3305e10f9e8a6655f5
Dick <dick@localhost.(none)>
2008-10-01 18:00:21 +0700

    change add to cell method to add

--- lib/gdata/blogger.rb
+++ lib/gdata/blogger.rb
@@ -1,65 +1,18 @@
-require 'gdata/client'
+require 'gdata/base'
 require 'builder'
-require 'hpricot'
-require 'rexml/document'
 
 module GData
 
-  class Blogger < GData::Client
-    attr_reader :blog_id, :entry_id, :blogs, :posts
-    # Default initialization method.  The blog ID and the entry ID may 
-    # or may not be known ahead of time.  Be sure to authenticate if needed.
-    def initialize(blog_id=nil, entry_id=nil)
+  class Blogger < GData::Base
+
+    def initialize(blog_id, entry_id=nil)
+      @blog_id = blog_id
+      @entry_id = entry_id
       super 'blogger', 'gdata-ruby', 'www.blogger.com'
-      @entry_id = entry_id unless entry_id==nil
-    end
-    
-    # Pull down a list of the user's blogs.  This allows the use of muliple
-    # blogs per user.  The @blogs Array will store the available blogs by
-    # internal hash. Requires user to be authenticated.
-    # ex. @blogs[0] = {"Blog Name" => "blog_id(a string of numbers)"}
-    # returns @blogs - an outer program can use this to set the blog id using
-    # the set_blog_id method.
-    def retrieve_blog_list
-      # retrieve the user's list of blogs from the server.
-      if authenticated? {
-      blog_feed = get('/feeds/default/blogs')
-      @blogs = Array.new
-      REXML::Document.new(blog_feed[1]).root.elements.each('entry') do |entry|
-        @blogs.push(entry.elements['id'].get_text.to_s.split(/blog-/).last)
-      end
-      set_blog_id(@blogs[0]) # set the initial blog ID to the first blog.
-      @blogs}
-      else puts "Not authenticated."
-      end
-
     end
 
-    # Sets the current blog_id to the specified blog - use the @blogs variable
-    # to define the blog id!
-    # ex: b.set_blog_id(b.blogs[1])
-    def set_blog_id(blog)
-      @blog_id = blog
-      # Now we've changed blogs, but we also need to clear the entry id's and
-      # Post list.
-      @posts = nil
-      @entry_id = nil
-    end
-
-    # Retrieves the post feed from the blog contained in @blog_id.  Run through
-    # REXML, it returns an array of the different id's of that blog's posts.
-    def get_post_feed
-      post_feed = get "/feeds/#{@blog_id}/posts/default"
-      @posts = Array.new
-      REXML::Document.new(post_feed[1]).elements.each('feed/entry') do |entry|
-        @posts.push(entry.elements['id'].get_text.to_s.split(/post-/).last)
-      end
-    end
-    
-    # Sets the post (entry) id to the one specified.  Retrieve from the @posts
-    # variable.
-    def set_post_id(post)
-      @entry_id = post
+    def feed
+      request "/feeds/#{@blog_id}/posts/default"
     end
 
     def entry
@@ -104,9 +57,9 @@ module GData
         end
       end
       
-      @entry ||= x.target!
+      @entry ||= x
       path = "/feeds/#{@blog_id}/posts/default"
-      post(path, @entry)
+      post(path, entry.to_s)
     end
 
   end
--- lib/gdata/spreadsheet.rb
+++ lib/gdata/spreadsheet.rb
@@ -1,39 +1,55 @@
-require 'gdata/client'
+require File.dirname(__FILE__)+'/base'
 
 module GData
 
-  class Spreadsheet < GData::Client
-
+  class Spreadsheet < GData::Base
+    attr_accessor :worksheet_id
+    
     def initialize(spreadsheet_id)
       @spreadsheet_id = spreadsheet_id
+      @worksheet_id = 1
       super 'wise', 'gdata-ruby', 'spreadsheets.google.com'
     end
 
     def evaluate_cell(cell)
-      path = "/feeds/cells/#{@spreadsheet_id}/1/#{@headers ? "private" : "public"}/basic/#{cell}"
-
+      path = "/feeds/cells/#{@spreadsheet_id}/#{@worksheet_id}/#{@headers ? "private" : "public"}/basic/#{cell}"
+      request(path)
       doc = Hpricot(request(path))
       result = (doc/"content[@type='text']").inner_html
     end
 
     def save_entry(entry)
-      path = "/feeds/cells/#{@spreadsheet_id}/1/#{@headers ? 'private' : 'public'}/full"
-
+      path = "/feeds/cells/#{@spreadsheet_id}/#{@worksheet_id}/#{@headers ? 'private' : 'public'}/full"
       post(path, entry)
     end
 
-    def entry(formula, row=1, col=1)
+    def entry(data)
+      value = @formula ? '='+data : data
       <<XML
-  <entry xmlns='http://www.w3.org/2005/Atom' xmlns:gs='http://schemas.google.com/spreadsheets/2006'>
-    <gs:cell row='#{row}' col='#{col}' inputValue='=#{formula}' />
+  <entry xmlns="http://www.w3.org/2005/Atom" xmlns:gs="http://schemas.google.com/spreadsheets/2006">
+    <gs:cell row="#{@row}" col="#{@col}" inputValue="#{value}" />
   </entry>
 XML
     end
-
-    def add_to_cell(formula)
-      save_entry(entry(formula))
+    
+    # def add_to_cell(data, cell, options = {})
+    #   convert_to_row_col(cell)
+    #   save_entry( entry(data, options) )
+    # end
+    
+    def add(data, options = {})
+      raise GData::Exceptions::Spreadsheet::UnspecifiedCell if options[:to].nil?
+      @cell = options[:to] 
+      @formula = ( options[:formula] || false ) 
+      convert_to_row_col
+      save_entry(entry(data))
     end
-
+    
+    def convert_to_row_col
+      @cell.match(/^R(\d+)C(\d+)$/)
+      @row, @col = $1, $2
+    end
+    
   end
 
 end
